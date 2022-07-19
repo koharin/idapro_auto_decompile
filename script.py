7d8654d9de48a4e7c198c577d81ee0da91faf8bb
koharin <emma01818@gmail.com>
2022-07-19 14:02:27 +0900

    Update script.py and result.json

--- script.py
+++ script.py
@@ -5,16 +5,13 @@ import json
 
 exclude_func = [
   '_start', 'deregister_tm_clones', 'register_tm_clones',
-  '__do_global_dtors_aux', 'frame_dummy', '__libc_csu_init', '__libc_csu_fini',
-  '_term_proc', '_init_proc']
+  '__do_global_dtors_aux', 'frame_dummy', '__libc_csu_init', '__libc_csu_fini']
 
-result=list()
-form = dict()
-def run_ida_decompiler(fn):
+def run_ida_decompiler():
     for seg_start in Segments():
-        if idc.get_segm_name(seg_start) == '.text':
+        if get_segm_name(seg_start) == '.text':
             for f_ea in Functions(seg_start, SegEnd(seg_start)):
-                f = idaapi.get_func(f_ea)
+                f = get_func(f_ea)
                 fname = GetFunctionName(f_ea)
                 if f is None:
                     print ("Failed to obtain the function@0x%08x" % (f_ea))
@@ -24,22 +21,29 @@ def run_ida_decompiler(fn):
                     df = decompile(f_ea)
                     if df is None:
                         print ("Failed to decompile the function %s" % fname)
-                    # print (df)
                     result.append(str(df))
                 except:
                     print ("Failed to attempt decompilation of the function@0x%08x" % (f_ea))
 
+result=list()
+form = dict()
 # get binary name
 fn = ida_nalt.get_root_filename()
-run_ida_decompiler(fn)
-buf = '\n'.join(result)
+form["binary_name"] = fn
+
+# run ida decompiler
+run_ida_decompiler()
+
+# json formatting
+buf = '\\n'.join(result)
 form["decompile_code"] = buf
 json_object = json.dumps(form, indent=4)
-with open('D:\\koharin\\Decompile\\result.json', 'a') as file: 
-    #file.write(buf)
+
+# save decompile code
+with open('D:\\koharin\\Decompile\\result.json', 'w') as file: 
     file.write(json_object)
-file.close()
+    file.close()
 
 # close IDA Pro 
-idc.exit()
-exit()
+# idc.exit()
+# exit()
