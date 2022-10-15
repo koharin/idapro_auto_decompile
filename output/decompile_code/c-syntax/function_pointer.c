59bfa5ca2bb824e1c026640e701c34440edb8b7a
Jeewoo Jung <35914056+koharin@users.noreply.github.com>
2022-10-15 15:06:13 +0900

    Update idapro decompile code

--- output/decompile_code/c-syntax/function_pointer.c
+++ output/decompile_code/c-syntax/function_pointer.c
@@ -1,13 +1,57 @@
-__int64 __fastcall sum(int a1, int a2)
+// positive sp value has been detected, the output may be wrong!
+void __fastcall __noreturn start(__int64 a1, __int64 a2, void (*a3)(void))
+{
+  __int64 v3; // rax
+  int v4; // esi
+  __int64 v5; // [rsp-8h] [rbp-8h] BYREF
+  char *retaddr; // [rsp+0h] [rbp+0h] BYREF
+
+  v4 = v5;
+  v5 = v3;
+  _libc_start_main(main, v4, &retaddr, 0LL, 0LL, a3, &v5);
+  __halt();
+}
+
+char *sub_1090()
+{
+  return &byte_4010;
+}
+
+__int64 sub_10C0()
+{
+  return 0LL;
+}
+
+char *sub_1100()
+{
+  char *result; // rax
+
+  if ( !byte_4010 )
+  {
+    if ( &__cxa_finalize )
+      _cxa_finalize(off_4008);
+    result = sub_1090();
+    byte_4010 = 1;
+  }
+  return result;
+}
+
+// attributes: thunk
+__int64 sub_1140()
+{
+  return sub_10C0();
+}
+
+__int64 __fastcall sub_1149(int a1, int a2)
 {
   return (unsigned int)(a1 + a2);
 }
 
-int __cdecl main(int argc, const char **argv, const char **envp)
+__int64 __fastcall main(int a1, char **a2, char **a3)
 {
   unsigned int v3; // eax
 
-  v3 = sum(2, 2);
+  v3 = sub_1149(2, 2);
   printf("%d", v3);
-  return 0;
+  return 0LL;
 }
--- output/decompile_code/c-syntax/switch2.c
+++ output/decompile_code/c-syntax/switch2.c
@@ -1,10 +1,54 @@
-int __cdecl main(int argc, const char **argv, const char **envp)
+// positive sp value has been detected, the output may be wrong!
+void __fastcall __noreturn start(__int64 a1, __int64 a2, void (*a3)(void))
+{
+  __int64 v3; // rax
+  int v4; // esi
+  __int64 v5; // [rsp-8h] [rbp-8h] BYREF
+  char *retaddr; // [rsp+0h] [rbp+0h] BYREF
+
+  v4 = v5;
+  v5 = v3;
+  _libc_start_main(main, v4, &retaddr, 0LL, 0LL, a3, &v5);
+  __halt();
+}
+
+char *sub_10D0()
+{
+  return &byte_4010;
+}
+
+__int64 sub_1100()
+{
+  return 0LL;
+}
+
+char *sub_1140()
+{
+  char *result; // rax
+
+  if ( !byte_4010 )
+  {
+    if ( &__cxa_finalize )
+      _cxa_finalize(off_4008);
+    result = sub_10D0();
+    byte_4010 = 1;
+  }
+  return result;
+}
+
+// attributes: thunk
+__int64 sub_1180()
+{
+  return sub_1100();
+}
+
+__int64 __fastcall main(int a1, char **a2, char **a3)
 {
   int v4; // [rsp+4h] [rbp-Ch] BYREF
   unsigned __int64 v5; // [rsp+8h] [rbp-8h]
 
   v5 = __readfsqword(0x28u);
-  __isoc99_scanf(&unk_2004, &v4, envp);
+  __isoc99_scanf(&unk_2004, &v4, a3);
   switch ( v4 / 10 )
   {
     case 5:
@@ -26,5 +70,5 @@ int __cdecl main(int argc, const char **argv, const char **envp)
       puts("F");
       break;
   }
-  return 0;
+  return 0LL;
 }
